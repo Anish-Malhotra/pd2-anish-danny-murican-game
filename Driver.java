a3c61d93a328c834349b0d726aa27db6e98e2ddf
Danny Qiu <dqiu55@gmail.com>
2014-01-08 09:27:44 -0500

    Layout code

---
+++ Driver.java
@@ -0,0 +1,12 @@
+public class Driver {
+
+    public static void main(String[] args) {
+        while (gameRunning) {
+            
+        }
+    }
+
+    public void quit() {
+// gets out of game
+    }
+}
---
+++ Listener.java
@@ -0,0 +1,3 @@
+public class Listener {
+//Glbal listener to call methods when key pressed
+}
